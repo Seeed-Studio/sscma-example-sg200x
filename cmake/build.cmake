6315eacdcf8ab3cdfd783ea319edc90a639afe49
LynnL4 <lht856@foxmail.com>
2024-05-28 06:35:52 +0000

    feat: improvement of cmake system

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -1,3 +1,17 @@
+
+
+foreach(req IN LISTS REQUIREDS)
+    include(${ROOT_DIR}/components/${req}/CMakeLists.txt)
+endforeach()
+
+
+foreach(library_dir IN LISTS LIBRARY_DIRS)
+    link_directories(${library_dir})
+endforeach( )
+
 add_executable(${PROJECT_NAME} ${SRCS})
 target_include_directories(${PROJECT_NAME} PUBLIC ${INCLUDE_DIRS})
 target_include_directories(${PROJECT_NAME} PRIVATE ${PRIVATE_INCLUDE_DIRS})
+target_link_libraries(${PROJECT_NAME} PRIVATE ${LIBRARIES})
+
+
--- cmake/macro.cmake
+++ cmake/macro.cmake
@@ -1,11 +1,14 @@
 set(SRCS "")
 set(INCLUDE_DIRS "")
 set(PRIVATE_INCLUDE_DIRS "")
+set(REQUIREDS "")
+set(LIBRARIES "")
+set(LIBRARY_DIRS "")
 
-function(project_register)
+function(component_register)
     set(options)
     set(one_value_args)
-    set(multi_value_args SRCS INCLUDE_DIRS PRIVATE_INCLUDE_DIRS)
+    set(multi_value_args SRCS INCLUDE_DIRS PRIVATE_INCLUDE_DIRS REQUIREDS LIBRARIES LIBRARY_DIRS)
     
     cmake_parse_arguments(PROJECT_REGISTER "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
@@ -20,8 +23,25 @@ function(project_register)
     foreach(inc IN LISTS PROJECT_REGISTER_PRIVATE_INCLUDE_DIRS)
         list(APPEND PRIVATE_INCLUDE_DIRS ${inc})
     endforeach()
+
+    foreach(req IN LISTS PROJECT_REGISTER_REQUIREDS)
+        list(APPEND REQUIREDS ${req})
+    endforeach()
     
+
+    foreach(lib IN LISTS PROJECT_REGISTER_LIBRARIES)
+        list(APPEND LIBRARIES ${lib})
+    endforeach()
+    
+    foreach(lib_dir IN LISTS PROJECT_REGISTER_LIBRARY_DIRS)
+        list(APPEND LIBRARY_DIRS ${lib_dir})
+    endforeach()
+
     set(SRCS ${SRCS} PARENT_SCOPE)
     set(INCLUDE_DIRS ${INCLUDE_DIRS} PARENT_SCOPE)
     set(PRIVATE_INCLUDE_DIRS ${PRIVATE_INCLUDE_DIRS} PARENT_SCOPE)
+    set(REQUIREDS ${REQUIREDS} PARENT_SCOPE)
+    set(LIBRARIES ${LIBRARIES} PARENT_SCOPE)
+    set(LIBRARY_DIRS ${LIBRARY_DIRS} PARENT_SCOPE)
+
 endfunction()
