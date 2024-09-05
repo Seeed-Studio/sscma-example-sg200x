8517bd0ad5ea2356f7e6c86dcb18d5207c905236
LynnL4 <lht856@foxmail.com>
2024-09-05 07:57:44 +0000

    feat: [video] update default param

--- components/video/src/app_ipcam_paramparse.c
+++ components/video/src/app_ipcam_paramparse.c
@@ -226,7 +226,7 @@ static const APP_VENC_CHN_CFG_S venc_jpeg = {
     .VpssGrp = 0,
     .VpssChn = 0,
     .stJpegCodecParam = {
-        .quality = 90,
+        .quality = 20,
         .MCUPerECS = 0,
     },
     .enBindMode = VENC_BIND_DISABLE,
