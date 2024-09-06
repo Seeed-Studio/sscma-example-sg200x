b4fd2d86577ae37585920f5742a29930d724572c
Lesords <2385342343@qq.com>
2024-09-06 06:58:17 +0000

    chore: add a compile script file

---
+++ scripts/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SG200X_PATH=$PWD
+SG200X_BUILD_PATH=$SG200X_PATH/build/
+SG200X_INSTALL_PATH=$SG200X_PATH/install/
+SG200X_SUPERVISOR_PATH=$SG200X_PATH/solutions/supervisor/
+
+function build() {
+    cd $SG200X_PATH
+
+    if [ -d $SG200X_BUILD_PATH ]; then
+        rm -r $SG200X_BUILD_PATH
+    fi
+
+    mkdir $SG200X_BUILD_PATH && cd $SG200X_BUILD_PATH
+
+    cmake $SG200X_SUPERVISOR_PATH && cmake --build . -j3
+}
+
+function install() {
+    cd $SG200X_PATH
+
+    if [ -d $SG200X_INSTALL_PATH ]; then
+        rm -r $SG200X_INSTALL_PATH
+    fi
+
+    mkdir $SG200X_INSTALL_PATH && cd $SG200X_INSTALL_PATH
+
+    mkdir -p mnt/system/usr/bin/
+
+    cp -r $SG200X_SUPERVISOR_PATH/main/scripts/ mnt/system/usr/
+    cp -r $SG200X_SUPERVISOR_PATH/main/dist/ mnt/system/usr/
+    cp -r $SG200X_BUILD_PATH/supervisor mnt/system/usr/bin/
+    ln -s ./usr/bin/supervisor mnt/system/default_app
+
+    tar zcf $SG200X_BUILD_PATH/supervisor.tar.gz ./*
+    echo "finished"
+}
+
+build
+install
