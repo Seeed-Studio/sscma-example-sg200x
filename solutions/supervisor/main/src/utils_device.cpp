333ee8d19aed8cba2f415fc2565f6b44ebb90a38
Lesords <2385342343@qq.com>
2024-10-09 02:40:57 +0000

    fix: modify the field name of the service status interface to adapt to the front end

--- solutions/supervisor/main/src/utils_device.cpp
+++ solutions/supervisor/main/src/utils_device.cpp
@@ -215,9 +215,9 @@ int queryServiceStatus(HttpRequest* req, HttpResponse* resp) {
 
     response["code"] = 0;
     response["msg"] = "";
-    response["data"]["sscma-node"] = convertStatus(sscmaStatus);
-    response["data"]["node-red"] = noderedStarting ? SERVICE_STATUS_STARTING : convertStatus(noderedStatus);
-    response["data"]["upgrade"] = systemStatus;
+    response["data"]["sscmaNode"] = convertStatus(sscmaStatus);
+    response["data"]["nodeRed"] = noderedStarting ? SERVICE_STATUS_STARTING : convertStatus(noderedStatus);
+    response["data"]["system"] = systemStatus;
 
     return resp->Json(response);
 }
