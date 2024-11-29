aa6d7a7b42d4431066cc4cd0771f8d34aa9501a8
LynnL4 <lht856@foxmail.com>
2024-11-29 09:18:10 +0000

    feat: add export.py and eval_seg.py for convert model

---
+++ scripts/eval_seg.py
@@ -0,0 +1,469 @@
+import os
+import re
+import shlex
+import argparse
+import subprocess
+
+import cv2
+import numpy as np
+import onnxruntime as ort
+
+class Colors:
+    def __init__(self):
+        """Initialize colors as hex = matplotlib.colors.TABLEAU_COLORS.values()."""
+        hexs = (
+            "042AFF",
+            "0BDBEB",
+            "F3F3F3",
+            "00DFB7",
+            "111F68",
+            "FF6FDD",
+            "FF444F",
+            "CCED00",
+            "00F344",
+            "BD00FF",
+            "00B4FF",
+            "DD00BA",
+            "00FFFF",
+            "26C000",
+            "01FFB3",
+            "7D24FF",
+            "7B0068",
+            "FF1B6C",
+            "FC6D2F",
+            "A2FF0B",
+        )
+        self.palette = [self.hex2rgb(f"#{c}") for c in hexs]
+        self.n = len(self.palette)
+        self.pose_palette = np.array(
+            [
+                [255, 128, 0],
+                [255, 153, 51],
+                [255, 178, 102],
+                [230, 230, 0],
+                [255, 153, 255],
+                [153, 204, 255],
+                [255, 102, 255],
+                [255, 51, 255],
+                [102, 178, 255],
+                [51, 153, 255],
+                [255, 153, 153],
+                [255, 102, 102],
+                [255, 51, 51],
+                [153, 255, 153],
+                [102, 255, 102],
+                [51, 255, 51],
+                [0, 255, 0],
+                [0, 0, 255],
+                [255, 0, 0],
+                [255, 255, 255],
+            ],
+            dtype=np.uint8,
+        )
+
+    def __call__(self, i, bgr=False):
+        """Converts hex color codes to RGB values."""
+        c = self.palette[int(i) % self.n]
+        return (c[2], c[1], c[0]) if bgr else c
+
+    @staticmethod
+    def hex2rgb(h):
+        """Converts hex color codes to RGB values (i.e. default PIL order)."""
+        return tuple(int(h[1 + i : 1 + i + 2], 16) for i in (0, 2, 4))
+
+
+class YOLOv8Seg:
+    """YOLOv8 segmentation model."""
+
+    def __init__(self, model):
+        """
+        Initialization.
+
+        Args:
+            model (str): Path to the ONNX model.
+        """
+        self.model = model
+        if self.model.endswith(".onnx"):
+            # Build Ort session
+            self.session = ort.InferenceSession(
+                self.model,
+                providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
+                if ort.get_device() == "GPU"
+                else ["CPUExecutionProvider"],
+            )
+
+            # Numpy dtype: support both FP32 and FP16 onnx model
+            self.ndtype = np.half if self.session.get_inputs()[0].type == "tensor(float16)" else np.single
+
+            # Get model width and height(YOLOv8-seg only has one input)
+            self.model_height, self.model_width = [x.shape for x in self.session.get_inputs()][0][-2:]
+        elif self.model.endswith(".cvimodel"):
+            try:
+                output = subprocess.check_output(['model_tool', '--info', self.model], text=True)
+            except subprocess.CalledProcessError as e:
+                print("Error executing command:", e)
+                exit(1)
+            
+
+            inputs_match = re.search(r'inputs\s*:\s*(.*)', output)
+            outputs_match = re.search(r'outputs\s*:\s*(.*)', output)
+            
+            self.inputs = inputs_match.group(1).strip().split(',') if inputs_match else None
+            self.outputs = outputs_match.group(1).strip().split(',') if outputs_match else None
+            
+            tensor_map_pattern = r'(\d+)\s+(\d+)\s+\w+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+\S+\s+io_mem\s+images_raw'
+            tensor_map_match = re.search(tensor_map_pattern, output)
+            
+            if tensor_map_match:
+                _, _, N, H, W, C = map(int, tensor_map_match.groups())
+            else:
+                N = C = H = W = None
+             
+            self.model_height, self.model_width = H, W
+            
+            self.ndtype = np.single
+
+        # Load COCO class names
+        self.classes = [str(i)  for i in range(80)]
+        
+        
+        # Create a color palette
+        self.color_palette = Colors()
+
+    def __call__(self, im0, conf_threshold=0.4, iou_threshold=0.45, nm=32):
+        """
+        The whole pipeline: pre-process -> inference -> post-process.
+
+        Args:
+            im0 (Numpy.ndarray): original input image.
+            conf_threshold (float): confidence threshold for filtering predictions.
+            iou_threshold (float): iou threshold for NMS.
+            nm (int): the number of masks.
+
+        Returns:
+            boxes (List): list of bounding boxes.
+            segments (List): list of segments.
+            masks (np.ndarray): [N, H, W], output masks.
+        """
+        # Pre-process
+        
+        im, ratio, (pad_w, pad_h) = self.preprocess(im0)
+        preds = []
+
+        if self.model.endswith(".onnx"):
+            # Ort inference
+            preds = self.session.run(None, {self.session.get_inputs()[0].name: im})
+        elif self.model.endswith(".cvimodel"):
+            # cvinn inference
+            np.savez('.input.npz', im)
+
+            # try:
+            #     cmd = [
+            #         'model_runner',
+            #         '--model', self.model,
+            #         '--input', '.input.npz',
+            #         '--output', '.output.npz' 
+            #     ]
+            #     result = subprocess.run(cmd, check=True, text=True, capture_output=True)
+            # except subprocess.CalledProcessError as e:
+            #     print(f"An error occurred while executing the command: {e}")
+            #     print("Command error output:\n", e.stderr)
+            #     exit(1)
+            
+            data = np.load('.output.npz')
+            
+            for key in self.outputs:
+                output_array = data[key]
+           
+                if output_array.shape[-1] == 1:
+                    preds.append(np.squeeze(output_array, axis=-1))
+                else:
+                    preds.append(output_array)
+            
+        # Post-process
+        boxes, segments, masks = self.postprocess(
+            preds,
+            im0=im0,
+            ratio=ratio,
+            pad_w=pad_w,
+            pad_h=pad_h,
+            conf_threshold=conf_threshold,
+            iou_threshold=iou_threshold,
+            nm=nm,
+        )
+        return boxes, segments, masks
+
+    def preprocess(self, img):
+        """
+        Pre-processes the input image.
+
+        Args:
+            img (Numpy.ndarray): image about to be processed.
+
+        Returns:
+            img_process (Numpy.ndarray): image preprocessed for inference.
+            ratio (tuple): width, height ratios in letterbox.
+            pad_w (float): width padding in letterbox.
+            pad_h (float): height padding in letterbox.
+        """
+        # Resize and pad input image using letterbox() (Borrowed from Ultralytics)
+        shape = img.shape[:2]  # original image shape
+        new_shape = (self.model_height, self.model_width)
+ 
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+        ratio = r, r
+        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+        pad_w, pad_h = (new_shape[1] - new_unpad[0]) / 2, (new_shape[0] - new_unpad[1]) / 2  # wh padding
+        if shape[::-1] != new_unpad:  # resize
+            img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+        top, bottom = int(round(pad_h - 0.1)), int(round(pad_h + 0.1))
+        left, right = int(round(pad_w - 0.1)), int(round(pad_w + 0.1))
+        img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+
+        # Transforms: HWC to CHW -> BGR to RGB -> div(255) -> contiguous -> add axis(optional)
+        if self.model.endswith(".onnx"):
+            img = np.ascontiguousarray(np.einsum("HWC->CHW", img)[::-1], dtype=self.ndtype) / 255.0
+        elif self.model.endswith(".cvimodel"):
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        img_process = img[None] if len(img.shape) == 3 else img
+        return img_process, ratio, (pad_w, pad_h)
+
+    def postprocess(self, preds, im0, ratio, pad_w, pad_h, conf_threshold, iou_threshold, nm=32):
+        """
+        Post-process the prediction.
+
+        Args:
+            preds (Numpy.ndarray): predictions come from ort.session.run().
+            im0 (Numpy.ndarray): [h, w, c] original input image.
+            ratio (tuple): width, height ratios in letterbox.
+            pad_w (float): width padding in letterbox.
+            pad_h (float): height padding in letterbox.
+            conf_threshold (float): conf threshold.
+            iou_threshold (float): iou threshold.
+            nm (int): the number of masks.
+
+        Returns:
+            boxes (List): list of bounding boxes.
+            segments (List): list of segments.
+            masks (np.ndarray): [N, H, W], output masks.
+        """
+        x, protos = preds[0], preds[1]  # Two outputs: predictions and protos
+    
+        # Transpose dim 1: (Batch_size, xywh_conf_cls_nm, Num_anchors) -> (Batch_size, Num_anchors, xywh_conf_cls_nm)
+        x = np.einsum("bcn->bnc", x)
+    
+        # Predictions filtering by conf-threshold
+        x = x[np.amax(x[..., 4:-nm], axis=-1) > conf_threshold]
+            
+        # Create a new matrix which merge these(box, score, cls, nm) into one
+        # For more details about `numpy.c_()`: https://numpy.org/doc/1.26/reference/generated/numpy.c_.html
+        x = np.c_[x[..., :4], np.amax(x[..., 4:-nm], axis=-1), np.argmax(x[..., 4:-nm], axis=-1), x[..., -nm:]]
+
+        # NMS filtering
+        x = x[cv2.dnn.NMSBoxes(x[:, :4], x[:, 4], conf_threshold, iou_threshold)]
+
+        # Decode and return
+        if len(x) > 0:
+            # Bounding boxes format change: cxcywh -> xyxy
+            x[..., [0, 1]] -= x[..., [2, 3]] / 2
+            x[..., [2, 3]] += x[..., [0, 1]]
+
+            # Rescales bounding boxes from model shape(model_height, model_width) to the shape of original image
+            x[..., :4] -= [pad_w, pad_h, pad_w, pad_h]
+            x[..., :4] /= min(ratio)
+
+            # Bounding boxes boundary clamp
+            x[..., [0, 2]] = x[:, [0, 2]].clip(0, im0.shape[1])
+            x[..., [1, 3]] = x[:, [1, 3]].clip(0, im0.shape[0])
+
+            # Process masks
+            masks = self.process_mask(protos[0], x[:, 6:], x[:, :4], im0.shape)
+          
+
+            # Masks -> Segments(contours)
+            segments = self.masks2segments(masks)
+            return x[..., :6], segments, masks  # boxes, segments, masks
+        else:
+            return [], [], []
+
+    @staticmethod
+    def masks2segments(masks):
+        """
+        Takes a list of masks(n,h,w) and returns a list of segments(n,xy), from
+        https://github.com/ultralytics/ultralytics/blob/main/ultralytics/utils/ops.py.
+
+        Args:
+            masks (numpy.ndarray): the output of the model, which is a tensor of shape (batch_size, 160, 160).
+
+        Returns:
+            segments (List): list of segment masks.
+        """
+        segments = []
+        for x in masks.astype("uint8"):
+            c = cv2.findContours(x, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]  # CHAIN_APPROX_SIMPLE
+            if c:
+                c = np.array(c[np.array([len(x) for x in c]).argmax()]).reshape(-1, 2)
+            else:
+                c = np.zeros((0, 2))  # no segments found
+            segments.append(c.astype("float32"))
+        return segments
+
+    @staticmethod
+    def crop_mask(masks, boxes):
+        """
+        Takes a mask and a bounding box, and returns a mask that is cropped to the bounding box, from
+        https://github.com/ultralytics/ultralytics/blob/main/ultralytics/utils/ops.py.
+
+        Args:
+            masks (Numpy.ndarray): [n, h, w] tensor of masks.
+            boxes (Numpy.ndarray): [n, 4] tensor of bbox coordinates in relative point form.
+
+        Returns:
+            (Numpy.ndarray): The masks are being cropped to the bounding box.
+        """
+        n, h, w = masks.shape
+        x1, y1, x2, y2 = np.split(boxes[:, :, None], 4, 1)
+        r = np.arange(w, dtype=x1.dtype)[None, None, :]
+        c = np.arange(h, dtype=x1.dtype)[None, :, None]
+        return masks * ((r >= x1) * (r < x2) * (c >= y1) * (c < y2))
+
+    def process_mask(self, protos, masks_in, bboxes, im0_shape):
+        """
+        Takes the output of the mask head, and applies the mask to the bounding boxes. This produces masks of higher
+        quality but is slower, from https://github.com/ultralytics/ultralytics/blob/main/ultralytics/utils/ops.py.
+
+        Args:
+            protos (numpy.ndarray): [mask_dim, mask_h, mask_w].
+            masks_in (numpy.ndarray): [n, mask_dim], n is number of masks after nms.
+            bboxes (numpy.ndarray): bboxes re-scaled to original image shape.
+            im0_shape (tuple): the size of the input image (h,w,c).
+
+        Returns:
+            (numpy.ndarray): The upsampled masks.
+        """
+        c, mh, mw = protos.shape
+
+        masks = np.matmul(masks_in, protos.reshape((c, -1))).reshape((-1, mh, mw)).transpose(1, 2, 0)  # HWN
+   
+        masks = np.ascontiguousarray(masks)
+        masks = self.scale_mask(masks, im0_shape)  # re-scale mask from P3 shape to original input image shape
+        masks = np.einsum("HWN -> NHW", masks)  # HWN -> NHW
+        masks = self.crop_mask(masks, bboxes)
+        return np.greater(masks, 0.5)
+
+    @staticmethod
+    def scale_mask(masks, im0_shape, ratio_pad=None):
+        """
+        Takes a mask, and resizes it to the original image size, from
+        https://github.com/ultralytics/ultralytics/blob/main/ultralytics/utils/ops.py.
+
+        Args:
+            masks (np.ndarray): resized and padded masks/images, [h, w, num]/[h, w, 3].
+            im0_shape (tuple): the original image shape.
+            ratio_pad (tuple): the ratio of the padding to the original image.
+
+        Returns:
+            masks (np.ndarray): The masks that are being returned.
+        """
+        im1_shape = masks.shape[:2]
+        if ratio_pad is None:  # calculate from im0_shape
+            gain = min(im1_shape[0] / im0_shape[0], im1_shape[1] / im0_shape[1])  # gain  = old / new
+            pad = (im1_shape[1] - im0_shape[1] * gain) / 2, (im1_shape[0] - im0_shape[0] * gain) / 2  # wh padding
+        else:
+            pad = ratio_pad[1]
+
+        # Calculate tlbr of mask
+        top, left = int(round(pad[1] - 0.1)), int(round(pad[0] - 0.1))  # y, x
+        bottom, right = int(round(im1_shape[0] - pad[1] + 0.1)), int(round(im1_shape[1] - pad[0] + 0.1))
+        if len(masks.shape) < 2:
+            raise ValueError(f'"len of masks shape" should be 2 or 3, but got {len(masks.shape)}')
+        masks = masks[top:bottom, left:right]
+        print(type(masks))
+        masks = cv2.resize(
+            masks, (im0_shape[1], im0_shape[0]), interpolation=cv2.INTER_LINEAR
+        )  # INTER_CUBIC would be better
+        cv2.imwrite("mask.jpg", masks)
+        if len(masks.shape) == 2:
+            masks = masks[:, :, None]
+        return masks
+
+    def draw_and_visualize(self, im, bboxes, segments, vis=False, save=True):
+        """
+        Draw and visualize results.
+
+        Args:
+            im (np.ndarray): original image, shape [h, w, c].
+            bboxes (numpy.ndarray): [n, 4], n is number of bboxes.
+            segments (List): list of segment masks.
+            vis (bool): imshow using OpenCV.
+            save (bool): save image annotated.
+
+        Returns:
+            None
+        """
+        # Draw rectangles and polygons
+        im_canvas = im.copy()
+        for (*box, conf, cls_), segment in zip(bboxes, segments):
+            
+            # draw contour and fill mask
+            cv2.polylines(im, np.int32([segment]), True, (255, 255, 255), 2)  # white borderline
+            cv2.fillPoly(im_canvas, np.int32([segment]), self.color_palette(int(cls_), bgr=True))
+
+            # draw bbox rectangle
+            cv2.rectangle(
+                im,
+                (int(box[0]), int(box[1])),
+                (int(box[2]), int(box[3])),
+                self.color_palette(int(cls_), bgr=True),
+                1,
+                cv2.LINE_AA,
+            )
+   
+            cv2.putText(
+                im,
+                f"{self.classes[cls_.astype(int)]}: {conf:.3f}",
+                (int(box[0]), int(box[1] - 9)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                self.color_palette(int(cls_), bgr=True),
+                2,
+                cv2.LINE_AA,
+            )
+
+        # Mix image
+        im = cv2.addWeighted(im_canvas, 0.3, im, 0.7, 0)
+
+        # Show image
+        if vis:
+            cv2.imshow("demo", im)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        # Save image
+        if save:
+            cv2.imwrite("demo.jpg", im)
+
+
+if __name__ == "__main__":
+    # Create an argument parser to handle command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True, help="Path to ONNX model")
+    parser.add_argument("--source", type=str, default=str("bus.jpg"), help="Path to input image")
+    parser.add_argument("--conf", type=float, default=0.25, help="Confidence threshold")
+    parser.add_argument("--iou", type=float, default=0.45, help="NMS IoU threshold")
+    args = parser.parse_args()
+
+    # Build model
+    model = YOLOv8Seg(args.model)
+
+    # Read image by OpenCV
+    img = cv2.imread(args.source)
+    
+
+    # Inference
+    boxes, segments, _ = model(img, conf_threshold=args.conf, iou_threshold=args.iou)
+    
+    # Draw bboxes and polygons
+    if len(boxes) > 0:
+        model.draw_and_visualize(img, boxes, segments, vis=False, save=True)
---
+++ scripts/export.py
@@ -0,0 +1,307 @@
+import os
+import shlex
+import argparse
+import subprocess
+import ast
+
+import onnx
+import onnxoptimizer
+
+import numpy as np
+
+def args_parser():
+    parser = argparse.ArgumentParser(description="A CLI tool for configuring model parameters.")
+
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Path to the model file.'
+    )
+    
+    parser.add_argument(
+        '--input_shapes',
+        type=lambda x: ast.literal_eval(x),
+        default=[[1,3,640,640]],
+        help='Input shapes as a multidimensional array, e.g., "[[1,3,640,640]]".'
+    )
+    
+    parser.add_argument(
+        '--mean',
+        type=lambda x: list(map(float, x.split(','))),
+        default=[0.0, 0.0, 0.0],
+        help='Mean values separated by commas, e.g., "0.0,0.0,0.0".'
+    )
+
+    parser.add_argument(
+        '--scale',
+        type=lambda x: list(map(float, x.split(','))),
+        default=[0.0039216, 0.0039216, 0.0039216],
+        help='Scale values separated by commas, e.g., "0.0039216,0.0039216,0.0039216".'
+    )
+
+    parser.add_argument(
+        '--pixel_format',
+        type=str,
+        default='rgb',
+        choices=['rgb', 'bgr', 'gray'],
+        help='Pixel format, e.g., "rgb".'
+    )
+
+    parser.add_argument(
+        '--output_names',
+        type=lambda x: [s.strip() for s in x.split(',')],
+        default=None,
+        help='Output names separated by commas, e.g., "output1,output2".'
+    )
+    
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        default=os.path.dirname(os.path.realpath(__file__)) + '/image',
+        help='Path to the dataset.'
+    )
+    
+    parser.add_argument(
+        '--test_input',
+        type=str,
+        default=os.path.dirname(os.path.realpath(__file__)) + '/image/pose_256_192.jpg',
+        help='Path to the test image.'
+    )
+
+    parser.add_argument(
+        '--epoch',
+        type=int,
+        default=200,
+        help='Number of epochs, e.g., 100.'
+    )
+    
+    parser.add_argument(
+        '--hybrid',
+        action='store_true',
+        default=False,
+        help='Whether to use hybrid quantization, e.g., True.'
+    )
+
+    parser.add_argument(
+        '--quantize',
+        type=str,
+        default='INT8',
+        choices=['F32', 'BF16', 'F16', 'INT8', 'INT4', 'W8F16', 'W8BF16', 'W4F16', 'W4BF16', 'F8E4M3', 'F8E5M2', 'QDQ'],
+        help='Quantization type, e.g., "INT8".'
+    )
+    
+    parser.add_argument(
+        '--quantize_table',
+        type=str,
+        default=None,
+        help='Quantization table path, e.g., "./quantize_table".'
+    )
+
+    parser.add_argument(
+        '--chip',
+        type=str,
+        default='cv181x',
+        choices=['bm1688', 'bm1684x', 'bm1684', 'bm1690', 'mars3', 'cv183x', 'cv182x', 'cv181x', 'cv180x', 'cv186x', 'cpu'],
+        help='Target chip, e.g., "cv181x".'
+    )
+
+    parser.add_argument(
+        '--quant_input',
+        type=bool,
+        default=True,
+        help='Whether to quantize input, e.g., True.'
+    )
+    
+    parser.add_argument(
+        '--work_dir',
+        type=str,
+        default='./work_dir',
+        help='Work directory, e.g., "./work_dir".'
+    )
+
+    args = parser.parse_args()
+    
+    return args
+
+model_name = ""
+model_path = ""
+target_dir = ""
+
+def model_transform(args):
+    
+    global model_name
+    global model_path
+    global target_dir
+    
+    try:
+        print("Model Transforming...")
+        cmd =[
+            'model_transform',
+            '--model_name', model_name,
+            '--model_def', model_path,
+            '--input_shapes', str(args.input_shapes).replace(' ', ''),
+            '--mean', ','.join(map(str, args.mean)),
+            '--scale', ','.join(map(str, args.scale)),
+            '--keep_aspect_ratio',
+            '--pixel_format', args.pixel_format,
+            '--output_names', ','.join(map(str, args.output_names)),
+            '--test_input', args.test_input,
+            '--test_result', model_name + '_top_outputs.npz',
+            '--mlir', model_name + '.mlir',
+        ]
+        print(shlex.join(cmd))
+        result = subprocess.run(cmd, check=True, text=True, capture_output=True)
+        print("Command output:\n", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while executing the command: {e}")
+        print("Command error output:\n", e.stderr)
+        print("Command error code:",e.stdout)
+        exit(1)
+        
+def model_quantize(args):
+        
+    global model_name
+    global model_path
+    global target_dir
+    
+    try:
+        print("Model Quantizing...")
+        cmd = [
+        'run_calibration', model_name + '.mlir',
+        '--dataset', args.dataset,
+        "--input_num", str(args.epoch),
+        "-o", model_name + '_calib_table',
+        ]
+        print(shlex.join(cmd))
+        result = subprocess.run(cmd, check=True, text=True, capture_output=True)
+        print("Command output:\n", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while executing the command: {e}")
+        print("Command error output:\n", e.stderr)
+        exit(1)
+        
+        
+def model_qtable(args):
+        
+    global model_name
+    global model_path
+    global target_dir
+    
+    try:
+        print("Model Hybrid Quantizing...")
+        cmd = [
+        'run_qtable', model_name + '.mlir',
+        '--dataset', args.dataset,
+        "--calibration_table", model_name + '_calib_table',
+        "--chip", args.chip,
+        "-o", model_name + '_qtable',
+        ]
+        print(shlex.join(cmd))
+        result = subprocess.run(cmd, check=True, text=True, capture_output=True)
+        print("Command output:\n", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while executing the command: {e}")
+        print("Command error output:\n", e.stderr)
+        exit(1)
+        
+        
+def model_depoly(args):
+        
+    global model_name
+    global model_path
+    global target_dir
+    
+    try:
+        print("Model Deploying...")
+        cmd = [
+        'model_deploy',
+        '--mlir', model_name + '.mlir',
+        '--quantize', args.quantize,
+        '--quant_input',
+        '--processor', args.chip,
+        '--calibration_table', model_name + '_calib_table',
+        '--test_input', args.test_input,
+        '--test_reference', model_name + '_top_outputs.npz',
+        '--customization_format', 'RGB_PACKED',
+        '--fuse_preprocess',
+        '--aligned_input']
+        if args.quantize_table != None:
+            cmd.append('--quantize_table')
+            cmd.append(args.quantize_table)
+            cmd.append('--model')
+            cmd.append(model_name + '_'  + args.chip + '_mix.cvimodel')
+        elif os.path.exists(model_name + '_qtable'):
+            cmd.append('--quantize_table')
+            cmd.append(model_name + '_qtable')
+            cmd.append('--model')
+            cmd.append(model_name + '_'  + args.chip + '_mix.cvimodel')
+        else:
+            cmd.append('--model')
+            cmd.append(model_name + '_'  + args.chip + '_' + args.quantize.lower() + '.cvimodel')
+        print(shlex.join(cmd))
+        result = subprocess.run(cmd, check=True, text=True, capture_output=True)
+        print("Command output:\n", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while executing the command: {e}")
+        print("Command error output:\n", e.stderr)
+        exit(1)
+    
+def main():
+        
+    global model_name
+    global model_path
+    
+    args = args_parser()       
+       
+    if args.output_names == None:
+        if args.model.split('.')[-1] == 'onnx':
+            model = onnx.load(args.model)
+            graph = model.graph
+            args.output_names = [output.name for output in graph.output]
+        
+    if args.output_names == None:
+        print("Error: Output names not specified.")
+        exit(1)
+        
+
+    print("Model:", args.model)
+    print("Work Directory:", args.work_dir)
+    print("Input Shapes:", args.input_shapes)
+    print("Mean Values:", args.mean)
+    print("Scale Values:", args.scale)
+    print("Pixel Format:", args.pixel_format)
+    print("Output Names:", args.output_names)
+    print("Epoch:", args.epoch)
+    print("Quantization Type:", args.quantize)
+    print("Chip:", args.chip)
+    print("Quantize Input:", args.quant_input)
+    print("Quantize Table:", args.quantize_table)
+    
+    model_name = os.path.splitext(os.path.split(args.model)[-1])[0]
+    model_path = os.path.realpath(args.model)
+    
+    target_dir = args.work_dir + '/' + model_name
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+        
+    if args.quantize_table != None:
+        args.quantize_table = os.path.realpath(args.quantize_table)
+    
+    if args.test_input != None:
+        args.test_input = os.path.realpath(args.test_input)
+    
+    if args.dataset != None:
+        args.dataset = os.path.realpath(args.dataset)
+        
+    os.chdir(target_dir)
+    
+    model_transform(args)
+    model_quantize(args)
+    
+    if args.hybrid:
+        model_qtable(args)
+        
+    model_depoly(args)
+
+if __name__ == "__main__":
+    main()
