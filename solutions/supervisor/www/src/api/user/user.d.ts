980e933baef6535ffe1a605f3abd4ee599a90032
chenwenhao <chenwenhao>
2025-04-29 16:33:09 +0800

    feat: [supervisor] add ssh toggle and login retry limit.

--- solutions/supervisor/www/src/api/user/user.d.ts
+++ solutions/supervisor/www/src/api/user/user.d.ts
@@ -2,6 +2,7 @@ interface IUserInfo {
   userName: string;
   firstLogin: boolean; //默认true；表示第一次登录，如果是第一次登录，需要修改密码
   sshkeyList: ISshItem[];
+  sshEnabled: boolean; //是否开启ssh，默认false
 }
 interface ISshItem {
   id: string;
