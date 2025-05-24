5afb28556d59cdeca2f4967224806be55431d474
mingzq <north_sea@qq.com>
2025-05-24 10:46:32 +0000

    refactor: api_base

--- solutions/web_server/main/include/api_base.h
+++ solutions/web_server/main/include/api_base.h
@@ -29,14 +29,12 @@ typedef enum {
     API_STATUS_OK = 0,
     API_STATUS_NEXT,
     API_STATUS_ERROR,
+    API_STATUS_REPLY_FILE,
     API_STATUS_AUTHORIZED,
     API_STATUS_UNAUTHORIZED,
 } api_status_t;
-
-// typedef const json& request_t;
 typedef const mg_http_message* request_t;
 typedef json& response_t;
-
 typedef api_status_t (*api_handler_t)(request_t req, response_t res);
 
 class rest_api {
@@ -59,116 +57,149 @@ public:
     }
 };
 
-typedef struct {
-    string name;
-    string filename;
-    size_t len;
-    char* buf;
-} form_t;
-
-class api_base {
-protected:
-    static inline const string STR_OK = "OK";
-    static inline const json EMPTY_JSON = json::object();
-
+class http_interface {
 public:
-protected:
-    vector<unique_ptr<rest_api>> list;
-
-    void register_api(string uri, api_handler_t handler, bool no_auth = false)
-    {
-        list.emplace_back(make_unique<rest_api>(uri, handler, no_auth));
-    }
-
     static string get_uri(request_t req)
     {
         return string(req->uri.buf, req->uri.len);
     }
 
-    static string get_param(request_t req, string name)
+    static string get_host(request_t req, bool ip_only = true)
     {
-        struct mg_str v = mg_http_var(req->query, mg_str(name.c_str()));
-        return (v.len == 0) ? "" : string(v.buf, v.len);
+        string host = get_header_var(req, "Host");
+
+        if (ip_only) {
+            size_t pos = host.find(':');
+            if (pos != string::npos) {
+                host = host.substr(0, pos);
+            }
+        }
+        return host;
+    }
+
+    static string get_port(request_t req)
+    {
+        string hdr = get_header_var(req, "Host");
+        size_t pos = hdr.find(':');
+        return (pos != string::npos && pos < hdr.length() - 1)
+            ? hdr.substr(pos + 1)
+            : "";
+    }
+
+    static string get_http_var(request_t req, string param)
+    {
+        struct mg_str v = mg_http_var(req->query, mg_str(param.c_str()));
+        return string(v.buf, v.len);
     }
 
     static string get_header_var(request_t req, const char* name)
     {
         mg_str* hdr = mg_http_get_header((mg_http_message*)req, name);
-        return (hdr) ? string(hdr->buf, hdr->len) : "";
+        return (hdr == nullptr) ? "" : string(hdr->buf, hdr->len);
     }
 
-    static bool is_path_valid(string& path)
-    {
-        return mg_path_is_sane(mg_str(path.c_str()));
-    }
-
-    static bool is_multipart(request_t req)
+    static json parse_body(request_t req)
     {
         string type = get_header_var(req, "Content-Type");
-        return (type.find("multipart/form-data") != string::npos);
+        if (type.find("application/json") == string::npos) {
+            return json();
+        }
+        return (req->body.len == 0) ? json() : json::parse(req->body.buf, req->body.buf + req->body.len);
     }
 
-    static bool get_body(request_t req, json& body)
+    static string get_param(request_t req, string param)
     {
-        body = (req->body.len) ? json::parse(req->body.buf) : json();
-        return body.empty() ? false : true;
+        auto&& val = get_http_var(req, param);
+        if (!val.empty()) {
+            return val;
+        }
+        return get_header_var(req, param.c_str());
     }
 
-    static bool get_form(request_t req, form_t& form, string name, string filename = "")
+    typedef struct {
+        string name;
+        string filename;
+        char* data;
+        size_t len;
+    } multipart_t;
+
+    static vector<multipart_t> get_multiparts(request_t req, const string& param = "")
     {
-        size_t pos = 0;
-        struct mg_http_part part;
+        vector<multipart_t> parts;
+        auto&& type = get_header_var(req, "Content-Type");
 
-        while ((pos = mg_http_next_multipart(req->body, pos, &part)) > 0) {
-            string _name = string(part.name.buf, part.name.len);
-            string _fname = string(part.filename.buf, part.filename.len);
+        if (type.find("multipart/form-data") != string::npos) {
+            size_t pos = 0;
+            struct mg_http_part part;
 
-            if (_name == name) {
-                if (!filename.empty() && _fname != filename) {
-                    continue;
-                }
-
-                form.name = _name;
-                form.filename = _fname;
-                form.len = part.body.len;
-                form.buf = part.body.buf;
-                return true;
+            while ((pos = mg_http_next_multipart(req->body, pos, &part)) > 0) {
+                multipart_t mp;
+                mp.name = string(part.name.buf, part.name.len);
+                mp.filename = string(part.filename.buf, part.filename.len);
+                mp.data = part.body.buf;
+                mp.len = part.body.len;
+                parts.emplace_back(mp);
+            }
+        } else {
+            if (!param.empty()) {
+                multipart_t mp;
+                mp.name = param;
+                mp.filename = get_param(req, param);
+                mp.data = req->body.buf;
+                mp.len = req->body.len;
+                parts.emplace_back(mp);
             }
         }
-        return false;
+
+        return parts;
     }
 
-    static api_status_t save_file(request_t req, string param,
-        string dir = "", string force_name = "")
+    static uint64_t uptime(void)
     {
-        string fname = get_param(req, param);
-        char* data = req->body.buf;
-        size_t len = req->body.len;
+        uint64_t uptime = 0;
+        std::ifstream uptime_file("/proc/uptime");
+        if (uptime_file.is_open()) {
+            double uptime_seconds;
+            uptime_file >> uptime_seconds;
+            uptime = static_cast<uint64_t>(uptime_seconds * 1000);
+            uptime_file.close();
+        }
+        return uptime;
+    }
 
-        if (is_multipart(req)) {
-            form_t form;
-            if (get_form(req, form, param)) {
-                fname = form.filename;
-                data = form.buf;
-                len = form.len;
-            }
-        }
+    static auto timestamp()
+    {
+        using namespace std::chrono;
+        auto now = system_clock::now();
+        auto timestamp = duration_cast<seconds>(now.time_since_epoch());
+        return timestamp.count();
+    }
+};
 
-        if (!force_name.empty()) {
-            fname = force_name;
-        }
-        if (fname.empty()) {
-            return API_STATUS_ERROR;
-        }
+#define REG_API_FULL(__uri, __handler, __no_auth) \
+    register_api(__uri, __handler, __no_auth)
+#define REG_API(__handler) \
+    REG_API_FULL(#__handler, __handler, false)
+#define REG_API_NO_AUTH(__handler) \
+    REG_API_FULL(#__handler, __handler, true)
 
-        string path = dir + fname;
-        if (!is_path_valid(path)) {
-            return API_STATUS_ERROR;
-        }
-        if (!write_file(path, data, len)) {
-            return API_STATUS_ERROR;
-        }
+class api_base : protected http_interface {
+protected:
+    static inline const string STR_OK = "OK";
+    static inline const string STR_FAILED = "Failed";
 
+    vector<unique_ptr<rest_api>> list;
+    void register_api(string uri, api_handler_t handler, bool no_auth = false)
+    {
+        list.emplace_back(make_unique<rest_api>(uri, handler, no_auth));
+    }
+
+private:
+    static api_status_t version(request_t req, response_t res)
+    {
+        res["uptime"] = uptime();
+        res["timestamp"] = timestamp();
+        response(res, 0, STR_OK, PROJECT_VERSION);
         return API_STATUS_OK;
     }
 
@@ -184,22 +215,30 @@ public:
             _script = script + " ";
             MA_LOGV(_group, ", ", _script);
 
-            register_api("version", [](request_t req, response_t res) {
-                res["code"] = 0;
-                res["msg"] = "";
-                res["data"] = PROJECT_VERSION;
-                res["uptime"] = 1234;
-                res["timestamp"] = 9999;
-                return API_STATUS_OK; }, true);
+            REG_API_NO_AUTH(version);
         }
     }
 
-    static void response(response_t res, int code,
-        const string& msg = STR_OK, const json& data = EMPTY_JSON)
+    static void response(response_t res, int code = 0,
+        const string& msg = STR_OK, const json& data = json({}))
     {
         res["code"] = code;
         res["msg"] = msg;
         res["data"] = data;
+    }
+
+    static void string_split(string s, const char delimiter, vector<string>& output)
+    {
+        size_t start = 0;
+        size_t end = s.find_first_of(delimiter);
+
+        while (end <= string::npos) {
+            output.emplace_back(s.substr(start, end - start));
+            if (end == string::npos)
+                break;
+            start = end + 1;
+            end = s.find_first_of(delimiter, start);
+        }
     }
 
     template <typename... Args>
@@ -217,7 +256,7 @@ public:
         MA_LOGV("Executing: ", full_cmd);
 
         if (!pipe) {
-            MA_LOGE("popen() failed: ", full_cmd, "errno=", errno, ", strerror=", strerror(errno));
+            MA_LOGE("popen() failed: ", cmd, "errno=", errno, ", strerror=", strerror(errno));
             throw std::runtime_error("popen() failed for: " + full_cmd);
         }
 
@@ -246,7 +285,7 @@ public:
                     MA_LOGE("read() error: ", strerror(errno), ", errno=", errno);
                     throw std::runtime_error("read() error during command execution");
                 }
-                usleep(100000); // 100ms
+                usleep(1000); // 1ms
             }
         }
 
@@ -258,7 +297,7 @@ public:
                 MA_LOGE("Command exited with status ", exit_status);
             }
         } else {
-            MA_LOGE("Command terminated abnormally: ", full_cmd);
+            MA_LOGE("Command terminated abnormally: ", cmd);
         }
 
         // Strip trailing newlines
@@ -274,97 +313,6 @@ public:
         MA_LOGV("Completed: [", result.size(), "] ", result);
         return result;
     }
-
-    static bool delete_file(const string& path)
-    {
-        namespace fs = std::filesystem;
-        try {
-            fs::path file_path(path);
-            if (!fs::exists(file_path)) {
-                MA_LOGE("File not found: ", path);
-                return false;
-            }
-            fs::remove(file_path);
-            return true;
-        } catch (const std::exception& e) {
-            MA_LOGE("File delete error: ", path, " - ", e.what());
-            return false;
-        }
-    }
-
-    static bool get_folder(const string& path, vector<string>& files)
-    {
-        namespace fs = std::filesystem;
-        try {
-            fs::path dir_path(path);
-            if (!fs::exists(dir_path) || !fs::is_directory(dir_path)) {
-                MA_LOGE("Directory not found: ", path);
-                return false;
-            }
-            for (const auto& entry : fs::directory_iterator(dir_path)) {
-                files.push_back(entry.path().filename());
-            }
-            return true;
-        } catch (const std::exception& e) {
-            MA_LOGE("Directory read error: ", path, " - ", e.what());
-            return false;
-        }
-    }
-
-    static string read_file(const string& path, const string& default_str = "")
-    {
-        namespace fs = std::filesystem;
-        try {
-            fs::path file_path(path);
-
-            if (!fs::exists(file_path)) {
-                MA_LOGE("File not found: ", path);
-                return default_str;
-            }
-
-            const auto file_size = fs::file_size(file_path);
-            std::ifstream ifs(file_path, std::ios::binary);
-            ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-
-            std::string buffer;
-            buffer.resize(file_size);
-
-            ifs.read(&buffer[0], file_size);
-            if (ifs.gcount() != static_cast<std::streamsize>(file_size)) {
-                MA_LOGE("Partial read: ", path, " (expected:", file_size, "actual: ", ifs.gcount(), ")");
-                return default_str;
-            }
-
-            return buffer;
-        } catch (const std::exception& e) {
-            MA_LOGE("File read error: ", path, " - ", e.what());
-            return default_str;
-        }
-    }
-
-    static bool write_file(const string& path, char* buf, size_t len)
-    {
-        namespace fs = std::filesystem;
-        try {
-            fs::path file_path(path);
-            fs::create_directories(file_path.parent_path());
-            std::ofstream ofs(file_path, std::ios::binary);
-            ofs.exceptions(std::ofstream::failbit | std::ofstream::badbit);
-            ofs.write(buf, len);
-            ofs.close();
-            return true;
-        } catch (const std::exception& e) {
-            MA_LOGE("File write error: ", path, " - ", e.what());
-            return false;
-        }
-    }
 };
-
-#define REG_API_FULL(__uri, __handler, __no_auth) \
-    register_api(__uri, __handler, __no_auth)
-#define REG_API(__handler) \
-    REG_API_FULL(#__handler, __handler, false)
-#define REG_API_NO_AUTH(__handler) \
-    REG_API_FULL(#__handler, __handler, true)
 
 #endif // API_BASE_H
